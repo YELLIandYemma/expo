025484bd02b956f530c65e443893e1225786efd1
Yelli <133573446+YELLIandYemma@users.noreply.github.com>
2023-12-18 18:01:17 +0500

    commited

---
+++ components/phoneCard.vue
@@ -0,0 +1,18 @@
+import type { PhoneCard } from '#build/components'; import type loginVue from
+'~/pages/login.vue';
+<template>
+  <div v-for="phone in phones">
+    <div>
+      <h1 class="text-white">{{ phone.name }}</h1>
+    </div>
+    <div>
+      <UButton size="sm">Button</UButton>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+const { data: phones } = await useAsyncData("phone_", async () =>
+  client.from("phone_").select("*")
+);
+console.log(phones.value);
+</script>
---
+++ pages/products.vue
@@ -0,0 +1,17 @@
+<template>
+  <PhoneCard />
+  <UButton size="sm" @click="handleSignout">Button</UButton>
+  <PhoneCard />
+</template>
+<script setup lang="ts">
+const supabase = useSupabaseClient();
+
+const handleSignout = async () => {
+  await supabase.auth.signOut();
+};
+const client = useSupabaseClient();
+const { data: phones } = await useAsyncData("phone_", async () =>
+  client.from("phone_").select("*")
+);
+refreshNuxtData();
+</script>
